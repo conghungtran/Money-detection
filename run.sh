3c8cdea7863e85ecf3deea07220a6123a44f7b11
conghungtran <tranconghung2023@gmail.com>
2025-12-23 10:28:57 +0700

    update  camera

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ pip install -r requirements.txt
 python3 train.py --data-dir dataset --output-dir artifacts --epochs 12 --batch-size 32
 
 # Predict
-python3 predict.py --model artifacts/best_model.pt --image dataset/000000/000000_000000_000000.jpg --topk 3
+python3 predict.py --model artifacts/best_model.pt --image dataset/000000/000000_0.jpg --topk 3
